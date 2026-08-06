78192647987064b064a364738e82a532d58abe8e
Fenix Games <syfa.adelia67@smp.belajar.id>
2026-08-06 10:46:55 -0300

    Align weekly interface contract with tabless Main

--- game/tests/test_weekly_main_interface_contract.gd
+++ game/tests/test_weekly_main_interface_contract.gd
@@ -3,12 +3,16 @@ extends Node
 func _ready() -> void:
     var main_script := FileAccess.get_file_as_string("res://scripts/ui/main.gd")
     var personal_script := FileAccess.get_file_as_string("res://scripts/ui/personal_screen.gd")
+    var personal_scene := FileAccess.get_file_as_string("res://scenes/PersonalScreen.tscn")
+    var arena_scene := FileAccess.get_file_as_string("res://scenes/ArenaScreen.tscn")
     var scene := FileAccess.get_file_as_string("res://scenes/Main.tscn")
 
     assert(scene.contains("text = \"Cerrar semana\""))
-    assert(scene.contains("Registro semanal"))
-    assert(scene.contains("Iniciar combate semanal"))
+    assert(scene.contains('[node name="ScreenHost" type="Control" parent="Margin/VBox"]'))
+    assert(not scene.contains('[node name="Tabs" type="TabContainer"'))
     assert(not scene.contains("Avanzar un día"))
+    assert(personal_scene.contains("Registro semanal"))
+    assert(arena_scene.contains('name="StartCombat"') or arena_scene.contains('name = "StartCombat"'))
 
     assert(main_script.contains("func _on_advance_week()"))
     assert(main_script.contains("GameState.advance_week()"))
