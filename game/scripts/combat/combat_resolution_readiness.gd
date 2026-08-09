d218189d399487efacfc904e0f96d9c46f97645f
Fenix Games <syfa.adelia67@smp.belajar.id>
2026-08-08 22:56:44 -0300

    feat(combat): split D5-D9 structural and numeric readiness

--- game/scripts/combat/combat_resolution_readiness.gd
+++ game/scripts/combat/combat_resolution_readiness.gd
@@ -1,18 +1,35 @@
 extends RefCounted
 
+const FROZEN_REQUIREMENTS: Array[String] = [
+	"target_rules",
+	"resolution_order",
+	"armor_and_vulnerability_structure",
+	"stamina_structure",
+	"accuracy_and_critical_structure",
+	"stat_scaling_roles",
+	"ko_structure",
+]
+
 const REQUIRED_DECISIONS: Array[String] = [
 	"damage_and_mitigation",
-	"armor_and_vulnerability",
-	"stamina_costs",
-	"accuracy_and_critical",
-	"stat_scaling",
-	"ko_and_surrender",
+	"armor_numeric_mitigation",
+	"armor_penetration",
+	"stamina_cost_table",
+	"stamina_recovery_amount",
+	"stamina_recovery_timing",
+	"accuracy_formula",
+	"stat_scaling_weights",
+	"surrender_rules",
 	"carryover",
 ]
 
 const CONDITIONAL_DECISIONS: Array[String] = [
 	"position_and_distance_model",
 ]
+
+
+func get_frozen_requirements() -> Array[String]:
+	return FROZEN_REQUIREMENTS.duplicate()
 
 
 func get_pending_requirements() -> Array[String]:
@@ -26,7 +43,7 @@ func get_conditional_requirements() -> Array[String]:
 func get_status() -> Dictionary:
 	return {
 		"frozen": false,
-		"frozen_requirements": ["target_rules", "resolution_order"],
+		"frozen_requirements": get_frozen_requirements(),
 		"pending_requirements": get_pending_requirements(),
 		"conditional_requirements": get_conditional_requirements(),
 	}
