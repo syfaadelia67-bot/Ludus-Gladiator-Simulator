5b6e274a542fc162e450cb0c6a3e7314dadae10e
Fenix Games <syfa.adelia67@smp.belajar.id>
2026-08-09 22:06:49 -0300

    style: apply gdformat to monthly main interface contract

--- game/tests/test_weekly_main_interface_contract.gd
+++ game/tests/test_weekly_main_interface_contract.gd
@@ -4,9 +4,7 @@ extends Node
 func _ready() -> void:
 	var hud_script := FileAccess.get_file_as_string("res://scripts/ui/unified_hud_shell.gd")
 	var hud_scene := FileAccess.get_file_as_string("res://scenes/UnifiedHudShell.tscn")
-	var closure := FileAccess.get_file_as_string(
-		"res://scripts/ui/weekly_closure_presenter.gd"
-	)
+	var closure := FileAccess.get_file_as_string("res://scripts/ui/weekly_closure_presenter.gd")
 	var personal_script := FileAccess.get_file_as_string("res://scripts/ui/personal_screen.gd")
 	var personal_scene := FileAccess.get_file_as_string("res://scenes/PersonalScreen.tscn")
 	var arena_scene := FileAccess.get_file_as_string("res://scenes/ArenaScreen.tscn")
@@ -14,20 +12,13 @@ func _ready() -> void:
 
 	assert(hud_scene.contains('text = "Cerrar mes"'))
 	assert(hud_scene.contains('text = "MES 1"'))
-	assert(
-		hud_scene.contains(
-			'[node name="AdvanceWeek" type="Button" parent="TopHUD/Margin/Row"]'
-		)
-	)
+	assert(hud_scene.contains('[node name="AdvanceWeek" type="Button" parent="TopHUD/Margin/Row"]'))
 	assert(main_scene.contains('[node name="ScreenHost" type="Control" parent="Margin/VBox"]'))
 	assert(not main_scene.contains('[node name="Tabs" type="TabContainer"'))
 	assert(not main_scene.contains("AdvanceDay"))
 	assert(not main_scene.contains("Avanzar un día"))
 	assert(personal_scene.contains("Registro mensual"))
-	assert(
-		arena_scene.contains('name="StartCombat"')
-		or arena_scene.contains('name = "StartCombat"')
-	)
+	assert(arena_scene.contains('name="StartCombat"') or arena_scene.contains('name = "StartCombat"'))
 
 	assert(hud_script.contains("@onready var advance_week_button"))
 	assert(hud_script.contains("GameState.month_advanced.connect"))
@@ -35,9 +26,7 @@ func _ready() -> void:
 	assert(hud_script.contains('week_summary.text = "MES %d"'))
 	assert(not hud_script.contains("GameState.week_advanced.connect"))
 	assert(not hud_script.contains("GameState.DAYS_PER_WEEK"))
-	assert(
-		closure.contains('const BUTTON_PATH := "UnifiedHudShell/TopHUD/Margin/Row/AdvanceWeek"')
-	)
+	assert(closure.contains('const BUTTON_PATH := "UnifiedHudShell/TopHUD/Margin/Row/AdvanceWeek"'))
 	assert(closure.contains("advance_button.pressed.connect(open_summary)"))
 	assert(closure.contains("GameState.advance_month()"))
 
