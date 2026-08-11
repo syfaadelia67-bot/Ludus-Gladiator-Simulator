ef56d4a5d752bb8bd29e8219013219cd2c8a5925
Fenix Games <syfa.adelia67@smp.belajar.id>
2026-08-11 01:33:05 -0300

    Remove frozen beast stats blocker from readiness test

--- game/tests/demo_pre_asset_readiness_test.gd
+++ game/tests/demo_pre_asset_readiness_test.gd
@@ -22,7 +22,7 @@ func _ready() -> void:
 
 func _assert_known_blockers(codes: Array[String]) -> void:
 	assert(not codes.has("rival_combat_v1_snapshots_missing"))
-	assert(codes.has("beast_combat_v1_stats_missing"))
+	assert(not codes.has("beast_combat_v1_stats_missing"))
 	assert(codes.has("beast_combat_v1_adapter_missing"))
 	assert(codes.has("building_upgrade_cost_pending:mine"))
 	assert(codes.has("canonical_skill_mechanics_not_frozen"))
