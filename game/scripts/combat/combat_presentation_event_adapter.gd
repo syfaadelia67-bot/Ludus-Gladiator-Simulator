67f33ed520c2e51da9613b5516261af7ac7eca30
Fenix Games <syfa.adelia67@smp.belajar.id>
2026-08-14 01:49:43 -0300

    Apply gdformat to combat presentation adapter

--- game/scripts/combat/combat_presentation_event_adapter.gd
+++ game/scripts/combat/combat_presentation_event_adapter.gd
@@ -6,11 +6,14 @@ func build_exchange_events(exchange_index: int, exchange_result: Dictionary) -> 
 	if str(exchange_result.get("status", "")) != "resolved":
 		return events
 
-	events.append(
-		{
-			"type": "exchange_started",
-			"exchange_index": exchange_index,
-		}
+	(
+		events
+		. append(
+			{
+				"type": "exchange_started",
+				"exchange_index": exchange_index,
+			}
+		)
 	)
 
 	for raw_intent in exchange_result.get("submitted_intents", []) as Array:
@@ -33,43 +36,52 @@ func build_exchange_events(exchange_index: int, exchange_result: Dictionary) -> 
 		if not raw_attack is Dictionary:
 			continue
 		var attack := raw_attack as Dictionary
-		events.append(
-			{
-				"type": "attack_resolved",
-				"exchange_index": exchange_index,
-				"actor_id": str(attack.get("actor_id", "")),
-				"target_id": str(attack.get("target_id", "")),
-				"action_id": str(attack.get("action_id", "")),
-				"skill_id": str(attack.get("skill_id", "")),
-				"hit": bool(attack.get("hit", false)),
-				"damage": int(attack.get("damage", 0)),
-				"defense_action_id": str(attack.get("defense_action_id", "")),
-				"intercepted": bool(attack.get("intercepted", false)),
-				"interceptor_id": str(attack.get("interceptor_id", "")),
-			}
+		(
+			events
+			. append(
+				{
+					"type": "attack_resolved",
+					"exchange_index": exchange_index,
+					"actor_id": str(attack.get("actor_id", "")),
+					"target_id": str(attack.get("target_id", "")),
+					"action_id": str(attack.get("action_id", "")),
+					"skill_id": str(attack.get("skill_id", "")),
+					"hit": bool(attack.get("hit", false)),
+					"damage": int(attack.get("damage", 0)),
+					"defense_action_id": str(attack.get("defense_action_id", "")),
+					"intercepted": bool(attack.get("intercepted", false)),
+					"interceptor_id": str(attack.get("interceptor_id", "")),
+				}
+			)
 		)
 
 	for raw_fighter_id in exchange_result.get("ko_fighter_ids", []) as Array:
 		var fighter_id := str(raw_fighter_id)
 		if fighter_id.is_empty():
 			continue
-		events.append(
-			{
-				"type": "fighter_knocked_out",
-				"exchange_index": exchange_index,
-				"fighter_id": fighter_id,
-			}
+		(
+			events
+			. append(
+				{
+					"type": "fighter_knocked_out",
+					"exchange_index": exchange_index,
+					"fighter_id": fighter_id,
+				}
+			)
 		)
 
 	if bool(exchange_result.get("combat_end_resolved", false)):
-		events.append(
-			{
-				"type": "combat_finished",
-				"exchange_index": exchange_index,
-				"outcome": str(exchange_result.get("outcome", "")),
-				"winner_team_id": str(exchange_result.get("winner_team_id", "")),
-				"loser_team_id": str(exchange_result.get("loser_team_id", "")),
-			}
+		(
+			events
+			. append(
+				{
+					"type": "combat_finished",
+					"exchange_index": exchange_index,
+					"outcome": str(exchange_result.get("outcome", "")),
+					"winner_team_id": str(exchange_result.get("winner_team_id", "")),
+					"loser_team_id": str(exchange_result.get("loser_team_id", "")),
+				}
+			)
 		)
 
 	return events
@@ -84,7 +96,8 @@ func get_contract() -> Dictionary:
 		"hit_math_allowed": false,
 		"winner_math_allowed": false,
 		"intent_selection_allowed": false,
-		"events": [
+		"events":
+		[
 			"exchange_started",
 			"action_declared",
 			"attack_resolved",
